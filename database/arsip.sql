91153d26dc2610a23543dfed18cbd0a1b88184c6
juanarap <juanfirdaus1010@gmail.com>
2022-11-23 12:45:49 +0700

    export

--- database/arsip.sql
+++ database/arsip.sql
@@ -1,212 +1,132 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 09 Nov 2022 pada 04.35
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 7.4.29
+/*
+SQLyog Ultimate v12.5.1 (64 bit)
+MySQL - 10.4.24-MariaDB : Database - arsip
+*********************************************************************
+*/
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+/*!40101 SET NAMES utf8 */;
 
+/*!40101 SET SQL_MODE=''*/;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`arsip` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
---
--- Database: `arsip`
---
+USE `arsip`;
 
--- --------------------------------------------------------
+/*Table structure for table `anggota` */
 
---
--- Struktur dari tabel `anggota`
---
+DROP TABLE IF EXISTS `anggota`;
 
 CREATE TABLE `anggota` (
-  `id_anggota` int(11) NOT NULL,
+  `id_anggota` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(100) NOT NULL,
   `tgl_lahir` date NOT NULL,
   `alamat` varchar(100) NOT NULL,
-  `no_tlp` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `no_tlp` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_anggota`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+/*Data for the table `anggota` */
 
---
--- Struktur dari tabel `arsip`
---
+/*Table structure for table `arsip` */
+
+DROP TABLE IF EXISTS `arsip`;
 
 CREATE TABLE `arsip` (
-  `id_arsip` int(11) NOT NULL,
+  `id_arsip` int(11) NOT NULL AUTO_INCREMENT,
   `nomor_surat` varchar(100) NOT NULL,
   `kategori` varchar(100) NOT NULL,
   `judul` varchar(200) NOT NULL,
   `waktu` datetime NOT NULL DEFAULT current_timestamp(),
-  `file` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `file` varchar(200) NOT NULL,
+  PRIMARY KEY (`id_arsip`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data untuk tabel `arsip`
---
+/*Data for the table `arsip` */
 
-INSERT INTO `arsip` (`id_arsip`, `nomor_surat`, `kategori`, `judul`, `waktu`, `file`) VALUES
-(1, '2020/PD3/TU/001', 'Undangan', 'Surat Pernyataan LDK', '2022-11-07 23:00:02', '328-SURAT PERNYATAAN LDK.pdf'),
-(2, '2020/PD2/TU/022', 'Undangan', 'Surat Izin ', '2022-11-07 23:00:49', '571-Surat Izin Ortu_Putri Eka Zulviana_2D.pdf');
+insert  into `arsip`(`id_arsip`,`nomor_surat`,`kategori`,`judul`,`waktu`,`file`) values 
+(5,'2022/SK/001','Pemberitahuan','Surat Keputusan','2022-11-23 11:58:00',''),
+(6,'2022/ST/002','Pemberitahuan','Surat Tugas','2022-11-23 11:58:36','');
 
--- --------------------------------------------------------
+/*Table structure for table `buku` */
 
---
--- Struktur dari tabel `buku`
---
+DROP TABLE IF EXISTS `buku`;
 
 CREATE TABLE `buku` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `gambar` varchar(100) NOT NULL,
   `judul` varchar(100) NOT NULL,
   `penulis` varchar(100) NOT NULL,
   `tahun_terbit` date NOT NULL,
   `penerbit` varchar(100) NOT NULL,
-  `genre` varchar(100) NOT NULL
+  `genre` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+/*Data for the table `buku` */
 
---
--- Struktur dari tabel `detail_pinjam`
---
+/*Table structure for table `detail_pinjam` */
+
+DROP TABLE IF EXISTS `detail_pinjam`;
 
 CREATE TABLE `detail_pinjam` (
   `tgl_pinjam` date NOT NULL,
   `tgl_kembali` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+/*Data for the table `detail_pinjam` */
 
---
--- Struktur dari tabel `login`
---
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
 
 CREATE TABLE `login` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data untuk tabel `login`
---
+/*Data for the table `login` */
 
-INSERT INTO `login` (`username`, `password`) VALUES
-('Admin', '123');
+insert  into `login`(`username`,`password`) values 
+('Admin','123');
 
--- --------------------------------------------------------
+/*Table structure for table `peminjaman` */
 
---
--- Struktur dari tabel `peminjaman`
---
+DROP TABLE IF EXISTS `peminjaman`;
 
 CREATE TABLE `peminjaman` (
-  `id_pinjam` int(11) NOT NULL,
+  `id_pinjam` int(11) NOT NULL AUTO_INCREMENT,
   `id_anggota` int(11) NOT NULL,
   `id` int(11) NOT NULL,
   `jumlah` int(11) NOT NULL,
-  `tgl_pinjam` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `tgl_pinjam` date NOT NULL,
+  PRIMARY KEY (`id_pinjam`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data untuk tabel `peminjaman`
---
+/*Data for the table `peminjaman` */
 
-INSERT INTO `peminjaman` (`id_pinjam`, `id_anggota`, `id`, `jumlah`, `tgl_pinjam`) VALUES
-(1, 0, 0, 3, '2022-11-02');
+insert  into `peminjaman`(`id_pinjam`,`id_anggota`,`id`,`jumlah`,`tgl_pinjam`) values 
+(1,0,0,3,'2022-11-02');
 
--- --------------------------------------------------------
+/*Table structure for table `pengembalian` */
 
---
--- Struktur dari tabel `pengembalian`
---
+DROP TABLE IF EXISTS `pengembalian`;
 
 CREATE TABLE `pengembalian` (
-  `id_kembali` int(11) NOT NULL,
+  `id_kembali` int(11) NOT NULL AUTO_INCREMENT,
   `id_anggota` int(11) NOT NULL,
   `id` int(11) NOT NULL,
   `id_pinjam` int(11) NOT NULL,
-  `tgl_kembali` date NOT NULL
+  `tgl_kembali` date NOT NULL,
+  PRIMARY KEY (`id_kembali`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
+/*Data for the table `pengembalian` */
 
---
--- Indeks untuk tabel `anggota`
---
-ALTER TABLE `anggota`
-  ADD PRIMARY KEY (`id_anggota`);
-
---
--- Indeks untuk tabel `arsip`
---
-ALTER TABLE `arsip`
-  ADD PRIMARY KEY (`id_arsip`);
-
---
--- Indeks untuk tabel `buku`
---
-ALTER TABLE `buku`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `peminjaman`
---
-ALTER TABLE `peminjaman`
-  ADD PRIMARY KEY (`id_pinjam`);
-
---
--- Indeks untuk tabel `pengembalian`
---
-ALTER TABLE `pengembalian`
-  ADD PRIMARY KEY (`id_kembali`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `anggota`
---
-ALTER TABLE `anggota`
-  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT untuk tabel `arsip`
---
-ALTER TABLE `arsip`
-  MODIFY `id_arsip` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT untuk tabel `buku`
---
-ALTER TABLE `buku`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `peminjaman`
---
-ALTER TABLE `peminjaman`
-  MODIFY `id_pinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT untuk tabel `pengembalian`
---
-ALTER TABLE `pengembalian`
-  MODIFY `id_kembali` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
